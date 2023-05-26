139883ecb0fcaa5271302be679a8b7800a8d51dd
Ralphie65 <ralphromano@atlantadental.com>
2023-05-26 15:49:52 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace DataSetTesting1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblPositive = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPositiveData = new System.Windows.Forms.Label();
+            this.lblNegativeData = new System.Windows.Forms.Label();
+            this.lblUnCategorizedData = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1425, 629);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblPositive
+            // 
+            this.lblPositive.AutoSize = true;
+            this.lblPositive.Location = new System.Drawing.Point(8, 46);
+            this.lblPositive.Name = "lblPositive";
+            this.lblPositive.Size = new System.Drawing.Size(94, 13);
+            this.lblPositive.TabIndex = 1;
+            this.lblPositive.Text = "Positive Reviews: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Negative Reviews: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(391, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Uncategorized :";
+            // 
+            // lblPositiveData
+            // 
+            this.lblPositiveData.AutoSize = true;
+            this.lblPositiveData.Location = new System.Drawing.Point(108, 46);
+            this.lblPositiveData.Name = "lblPositiveData";
+            this.lblPositiveData.Size = new System.Drawing.Size(10, 13);
+            this.lblPositiveData.TabIndex = 6;
+            this.lblPositiveData.Text = " ";
+            // 
+            // lblNegativeData
+            // 
+            this.lblNegativeData.AutoSize = true;
+            this.lblNegativeData.Location = new System.Drawing.Point(306, 46);
+            this.lblNegativeData.Name = "lblNegativeData";
+            this.lblNegativeData.Size = new System.Drawing.Size(10, 13);
+            this.lblNegativeData.TabIndex = 7;
+            this.lblNegativeData.Text = " ";
+            // 
+            // lblUnCategorizedData
+            // 
+            this.lblUnCategorizedData.AutoSize = true;
+            this.lblUnCategorizedData.Location = new System.Drawing.Point(479, 46);
+            this.lblUnCategorizedData.Name = "lblUnCategorizedData";
+            this.lblUnCategorizedData.Size = new System.Drawing.Size(10, 13);
+            this.lblUnCategorizedData.TabIndex = 8;
+            this.lblUnCategorizedData.Text = " ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "All",
+            "Positive",
+            "Negative",
+            "Uncategorized"});
+            this.comboBox1.Location = new System.Drawing.Point(1179, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1144, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Filter";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1430, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem1});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.aboutToolStripMenuItem.Text = "Go To";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(531, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(662, 70);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 13;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(743, 70);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 14;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1430, 759);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblUnCategorizedData);
+            this.Controls.Add(this.lblNegativeData);
+            this.Controls.Add(this.lblPositiveData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPositive);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Dash";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblPositive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPositiveData;
+        private System.Windows.Forms.Label lblNegativeData;
+        private System.Windows.Forms.Label lblUnCategorizedData;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using GemBox.Spreadsheet;
+
+
+
+namespace DataSetTesting1
+{
+    public partial class Main : Form
+    {
+        DataTable finalDT = new DataTable();
+        BindingSource bS = new BindingSource();
+        DataView dV = new DataView();
+        public Main()
+        {
+
+            InitializeComponent();
+            GemBox.Spreadsheet.SpreadsheetInfo.SetLicense("Free-Limited-Key");
+
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            string windowsUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            string positiveFileName = $"C:\\Users\\{windowsUser}\\Documents\\Positive.xlsx";
+            string negativeFileName = $"C:\\Users\\{windowsUser}\\Documents\\Negative.xlsx";
+            
+            
+            //string fileName = $"C:\\Users\\{windowsUser}\\Documents\\Reviews.xlsx";
+
+            MessageBox.Show("Please Select Positive Word list.xlsx");
+            OpenFileDialog opfd = new OpenFileDialog();
+            opfd.ShowDialog();
+            positiveFileName = opfd.FileName;
+            MessageBox.Show("Please Select Negative Word list.xlsx");
+            opfd.ShowDialog();
+            negativeFileName = opfd.FileName;
+            MessageBox.Show("Please Select List of Reviews.xlsx");
+            opfd.ShowDialog();
+            String fileName = opfd.FileName;
+
+            DataTable positiveDT = CreateWordDT(positiveFileName);
+            DataTable negativeDT = CreateWordDT(negativeFileName);
+            finalDT = Read_Excel(fileName, positiveDT, negativeDT);
+            bS.DataSource = finalDT;
+            dataGridView1.Columns[4].Width = 865;
+            comboBox1.SelectedItem = "ALL";
+
+        }
+        private DataTable CreateWordDT(string fileName)
+        {
+            var workbook = ExcelFile.Load(fileName);
+            var worksheet = workbook.Worksheets[0];
+            var rowCount = worksheet.Rows.Count;
+            var columnCount = worksheet.CalculateMaxUsedColumns();
+            var wordDT = new DataTable();
+            wordDT.PrimaryKey = new DataColumn[] { wordDT.Columns.Add("Word", typeof(string)) };
+            int rC = worksheet.Rows.Count;
+            for (int i = 0; i < rC; i++)
+            {
+                var value = worksheet.Cells[i, 0].Value?.ToString();
+                wordDT.Rows.Add(value);
+            }
+            return wordDT;
+        }
+        private int CheckIfPositiveOrNegative(DataTable positive, DataTable negative, string[] wordsInInput)
+        {
+            foreach (string word in wordsInInput)
+            {
+                if (positive.Rows.Contains(word))
+                {
+                    return 1;
+                }
+                else if (negative.Rows.Contains(word))
+                {
+                    return 2;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return 0;
+        }
+        private DataTable Read_Excel(string fileName, DataTable positive, DataTable negative)
+        {
+            int lblPositiveCount = 0;
+            int lblNegativeCount = 0;
+            int lblUnCategorizedCount = 0;
+            var workbook = ExcelFile.Load(fileName);
+            var worksheet = workbook.Worksheets[0];
+            var rowCount = worksheet.Rows.Count;
+            var columnCount = worksheet.CalculateMaxUsedColumns();
+            var dT = new DataTable();
+            dT.Columns.Add("ID", typeof(int));
+            dT.Columns.Add("Date", typeof(string));
+            dT.Columns.Add("Word_Count", typeof(int));
+            dT.Columns.Add("Rating", typeof(int));
+            dT.Columns.Add("Review", typeof(string));
+            dT.Columns.Add("Positive_Negative", typeof(int));
+            int rC = worksheet.Rows.Count;
+            for (int i = 1; i < rC; i++)
+            {
+                string iD = worksheet.Cells[i, 0].Value?.ToString();
+                string date = worksheet.Cells[i, 1].Value?.ToString();
+                string wordCount = worksheet.Cells[i, 2].Value?.ToString();
+                string rating = worksheet.Cells[i, 3].Value?.ToString();
+                string review = worksheet.Cells[i, 4].Value?.ToString();
+                string[] wordsInReview = review.Split(new[] { ' ', ',', '.', '!' }, StringSplitOptions.RemoveEmptyEntries);
+                int check = CheckIfPositiveOrNegative(positive, negative, wordsInReview);
+                switch (check)
+                {
+                    case 0:
+                        lblUnCategorizedCount += 1;
+                        lblUnCategorizedData.Text = lblUnCategorizedCount.ToString();
+                        break;
+                    case 1:
+                        lblPositiveCount += 1;
+                        lblPositiveData.Text = lblUnCategorizedCount.ToString();
+                        break;
+                    case 2:
+                        lblNegativeCount += 1;
+                        lblNegativeData.Text = lblNegativeCount.ToString();
+                        break;
+                }
+                dT.Rows.Add(iD, date, wordCount, rating, review, check);
+            }
+            dataGridView1.DataSource = dT;
+            return dT;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                return;
+            }
+            DataView dV = new DataView(finalDT);
+            if (comboBox1.SelectedItem.Equals("Positive"))
+            {
+
+                dV.RowFilter = $"[Positive_Negative] = '{1}'";
+                dataGridView1.DataSource = dV;
+            }
+            else if (comboBox1.SelectedItem.Equals("Negative"))
+            {
+                dV.RowFilter = $"[Positive_Negative] = '{2}'";
+                dataGridView1.DataSource = dV;
+            }
+            else if (comboBox1.SelectedItem.Equals("Uncategorized"))
+            {
+                dV.RowFilter = $"[Positive_Negative] = '{0}'";
+                dataGridView1.DataSource = dV;
+            }
+            else if (comboBox1.SelectedItem.Equals("All"))
+            {
+                dV.RowFilter = null;
+                dataGridView1.DataSource = (finalDT);
+                dataGridView1.Columns[4].Width = 865;
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string cboValue = comboBox1.SelectedItem.ToString();
+
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created By: Ralph Romano. Sources For Word Lists: Bing Liu, Minqing Hu and Junsheng Cheng.Opinion Observer: Analyzing and Comparing Opinions on the Web. Proceedings of the 14th International World Wide Web conference(WWW - 2005), May 10 - 14,  2005, Chiba, Japan");
+        }
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var filteredrows = finalDT.AsEnumerable()
+                    .Where(row => row
+                    .Field<string>("Review")
+                    .Contains(textBox1.Text))
+                    .CopyToDataTable();
+                dataGridView1.DataSource = filteredrows;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            dV.RowFilter = null;
+            dataGridView1.DataSource = (finalDT);
+            dataGridView1.Columns[4].Width = 865;
+        }
+    }
+
+}
